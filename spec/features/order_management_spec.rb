e3e848ddb4e6ed387929d2e9c2ab83b8342404aa
Tori <victoria.ashley.wood@gmail.com>
2018-11-15 20:42:33 -0700

    fix indents

--- spec/features/order_management_spec.rb
+++ spec/features/order_management_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 
 RSpec.feature 'Order management', :type => :feature do
   it 'User visits the Orders index page' do 
-  	order_id = create(:order).id
-  	visit orders_path
+    order_id = create(:order).id
+    visit orders_path
 
-  	click_link order_id
+    click_link order_id
 
-  	expect(current_path).to eq order_path(order_id)
-  	expect(page).to have_text("Order #{order_id}")
+    expect(current_path).to eq order_path(order_id)
+    expect(page).to have_text("Order #{order_id}")
   end
 end
